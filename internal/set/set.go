4cf41380a30c4a13d1674cc8dcf4938228ef7d54
user <haghee0eip6ohjoh@protonmail.com>
2022-08-03 01:00:43 +0500

    misc(set): rewrite set container

--- internal/set/set.go
+++ internal/set/set.go
@@ -1,10 +1,7 @@
 package set
 
 import (
-	"bytes"
-	"encoding/gob"
 	"math/rand"
-	"sync/atomic"
 	"time"
 )
 
@@ -14,6 +11,9 @@ func init() {
 	src := rand.NewSource(time.Now().Unix())
 	randomizer = rand.New(src)
 }
+
+func first[T1, T2 any](v1 T1, v2 T2) T1  { return v1 }
+func second[T1, T2 any](v1 T1, v2 T2) T2 { return v2 }
 
 // Zero-sized value used as a placeholder
 type _dummy = struct{}
@@ -27,137 +27,71 @@ type Set[T comparable] struct {
 	// useful when some action requires a sequence of elements,
 	// not their map representation
 	snap []T
-
-	locked int32
 }
 
-const (
-	unlockedSet = iota
-	lockedSet
-)
+func (s *Set[T]) Add(val T) bool {
+	_, ok := s.m[val]
+	s.m[val] = dummy
 
-// Internal check with no blocking
-func (s *Set[T]) containsUnsafe(v T) bool {
-	_, c := s.m[v]
-	return c
+	if s.snap != nil {
+		s.snap = append(s.snap, val)
+	}
+
+	return !ok
 }
 
-func (s *Set[T]) unlocked() bool {
-	return atomic.LoadInt32(&s.locked) == unlockedSet
+func (s *Set[T]) Has(val T) bool {
+	_, ok := s.m[val]
+	return ok
 }
 
-// Makes an internal snapshot only if it's necessary, i.e. the previous one is empty
-func (s *Set[T]) snapshot() {
+func (s *Set[T]) Delete(val T) {
+	delete(s.m, val)
+	s.snap = nil
+}
+
+func (s *Set[T]) checkSnap() {
 	if s.snap == nil {
-		s.mustSnapshot()
+		s.hardSnap()
+	} else if len(s.snap) != len(s.m) {
+		s.hardSnap()
 	}
 }
 
-// Makes an internal snapshot of a values for further usage.
-func (s *Set[T]) mustSnapshot() {
-	cd := make([]T, s.Len())
-	cx := 0
+func (s *Set[T]) hardSnap() {
+	s.snap = make([]T, 0, len(s.m))
+
 	for k := range s.m {
-		cd[cx] = k
-		cx++
+		s.snap = append(s.snap, k)
 	}
-	s.snap = cd
 }
 
-// Has determines that a key is in the set.
-func (s *Set[T]) Has(v T) bool {
-	return s.containsUnsafe(v)
+func (s *Set[T]) Clear() {
+	s.m = map[T]_dummy{}
+	s.snap = nil
 }
 
-// Add adds a string value to the set. If the string is already in the set, does nothing.
-// Returns exactly was the value added at this time or not.
-func (s *Set[T]) Add(v T) bool {
-	if !s.unlocked() {
-		return false
+func (s *Set[T]) Slice() []T {
+	slice := make([]T, 0, len(s.m))
+
+	for k := range s.m {
+		slice = append(slice, k)
 	}
 
-	c := s.containsUnsafe(v)
-
-	if !c && s.snap != nil {
-		s.snap = append(s.snap, v)
-	}
-
-	s.m[v] = dummy
-	return !c
+	return nil
 }
 
 func (s *Set[T]) Len() int {
 	return len(s.m)
 }
 
-func (s *Set[T]) Delete(v T) {
-	if !s.unlocked() {
-		return
-	}
-
-	delete(s.m, v)
-	s.snap = nil // invalidate cache
-}
-
-// Range calls f sequentially for each value present in the set.
-// If f returns false, range stops the iteration.
-// Range makes a snapshot of the values at the start.
-// Values added during f execution will not be iterated.
-func (s *Set[T]) Range(f func(T) bool) {
-	for k := range s.m {
-		if !f(k) {
-			break
-		}
-	}
-}
-
-// Pick picks a random element from the set and returns it.
-// If the set is empty, Pick panics.
-func (s *Set[T]) Pick() T {
-	s.snapshot()
-
-	if len(s.snap) == 0 {
-		panic("calling Pick() on empty Set")
-	}
-
-	return s.snap[randomizer.Intn(len(s.snap))]
-}
-
-func (s *Set[T]) ToSlice() []T {
-	ms := make([]T, len(s.m))
-	x := 0
-	for k := range s.m {
-		ms[x] = k
-		x++
-	}
-
-	return ms
-}
-
-// Clear removes everything from the set.
-// It is more convenient than creating a new one, if necessary.
-func (s *Set[T]) Clear() {
-	if !s.unlocked() {
-		return
-	}
-
-	s.snap = nil
-	s.m = make(map[T]_dummy)
-}
-
-// Equal reports if two *Set[T]s are equal by values, or they aren't.
 func (s *Set[T]) Equal(s1 *Set[T]) bool {
-	if s == s1 { // equal by pointer
-		return true
-	}
-
-	if len(s.m) != len(s1.m) {
+	if s.Len() != s1.Len() {
 		return false
 	}
 
 	for k := range s.m {
-		_, ok := s1.m[k]
-		if !ok {
+		if !s1.Has(k) {
 			return false
 		}
 	}
@@ -165,103 +99,35 @@ func (s *Set[T]) Equal(s1 *Set[T]) bool {
 	return true
 }
 
-// Intersection returns the set with the elements that contains the items that exist in both sets.
-func (s *Set[T]) Intersection(s1 *Set[T]) *Set[T] {
-	in := NewSet[T](nil)
-
-	s.Range(func(s T) bool {
-		if s1.Has(s) {
-			in.Add(s)
+func (s *Set[T]) Range(f func(T) bool) {
+	for k := range s.m {
+		if !f(k) {
+			return
 		}
-		return true
-	})
-
-	return in
+	}
 }
 
-// Difference returns a set containing the difference between two sets.
-func (s *Set[T]) Difference(s1 *Set[T]) *Set[T] {
-	in := NewSet[T](nil)
-
-	s.Range(func(s T) bool {
-		if !s1.Has(s) {
-			in.Add(s)
-		}
-		return true
-	})
-
-	s1.Range(func(v T) bool {
-		if !s.Has(v) {
-			in.Add(v)
-		}
-		return true
-	})
-
-	return in
-}
-
-// Copy creates an independent copy of the set.
-// It is equal to New*Set[T](s.ToSlice()), but this is more efficient.
 func (s *Set[T]) Copy() *Set[T] {
-	s.snapshot()
+	s.checkSnap()
 
-	return NewSet[T](s.snap)
+	return NewSet[T](s.snap...)
 }
 
-// If the set is locked, any write/delete/clear operation on it will result in nothing.
-// For example, Add(...) won't add a string when the set is locked.
-// Returns current locking state (true - locked).
-// This operation is goroutine-safe and does not require any synchronization stuff.
-func (s *Set[T]) ToggleLock() bool {
-	old := atomic.LoadInt32(&s.locked)
-	atomic.StoreInt32(&s.locked, old^lockedSet)
-	return old == unlockedSet
-}
-
-// Gob encoding implementation
-func (s *Set[T]) GobEncode() ([]byte, error) {
-	encb := new(bytes.Buffer)
-	enc := gob.NewEncoder(encb)
-	err := enc.Encode(s.ToSlice())
-	if err != nil {
-		return nil, err
+func (s *Set[T]) Pick() T {
+	if s.Len() == 0 {
+		panic("Pick() on empty Set!")
 	}
 
-	return encb.Bytes(), nil
+	s.checkSnap()
+
+	return s.snap[randomizer.Intn(len(s.snap))]
 }
 
-// Gob decoding implementation
-func (s *Set[T]) GobDecode(b []byte) error {
-	if !s.unlocked() {
-		return nil
-	}
+func NewSet[T comparable](init ...T) *Set[T] {
+	s := &Set[T]{m: map[T]_dummy{}}
 
-	s.Clear() // or maybe not?
-	defer s.snapm()
-	dec := gob.NewDecoder(bytes.NewReader(b))
-	return dec.Decode(&s.snap)
-}
-
-//
-func (s *Set[T]) snapm() {
-	for _, x := range s.snap {
-		s.m[x] = dummy
-	}
-}
-
-// Creates a new set from the values of a given slice.
-// Left nil if you want to create an empty set.
-func NewSet[T comparable](fromSlice []T) *Set[T] {
-	s := new(Set[T])
-	s.m = make(map[T]_dummy)
-
-	if fromSlice != nil {
-		s.snap = make([]T, len(fromSlice))
-		copy(s.snap, fromSlice)
-	}
-
-	for _, k := range fromSlice {
-		s.Add(k)
+	for _, val := range init {
+		s.Add(val)
 	}
 
 	return s
